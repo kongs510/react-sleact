7f67e0d58714cd5aa8079121fc594a2c18cb267a
공민철 <68903200+kongs510@users.noreply.github.com>
2021-08-20 14:06:50 +0900

    fix

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if (prod) {
     })
   );
 }
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -62,7 +62,7 @@ app.use(passport.session());
 
 app.use("/api", apiRouter);
 app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const server = app.listen(app.get("PORT"), () => {
